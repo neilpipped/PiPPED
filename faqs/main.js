a8e7df00fc32ecba97786eeec84486f4d9a83625
Neil Nguyen <94281344+neilpipped@users.noreply.github.com>
2023-01-18 23:44:01 +0800

    Add files via upload

--- faqs/main.js
+++ faqs/main.js
@@ -11,3 +11,11 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 }
+
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
