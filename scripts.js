991556b22dbef111950301e7680522428e8797b1
Neil Nguyen <94281344+neilpipped@users.noreply.github.com>
2023-01-22 22:24:10 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,17 +5,3 @@ var ready = (callback) => {
 ready(() => {
 	document.querySelector(".header").style.height = window.innerHeight + "px";
 })
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("main-navigation");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
